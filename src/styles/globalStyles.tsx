0b02cf602786ca2283fff1b073cca6b1d15b6bca
Maciej Hoffmann <maciej.hoffmann@blocklabs.eu>
2022-07-18 02:16:07 +0200

    init

---
+++ src/styles/globalStyles.tsx
@@ -0,0 +1,16 @@
+import * as styled from "styled-components";
+import ClaconTTF from "../fonts/clacon2.ttf";
+export default styled.createGlobalStyle`
+  @font-face {
+    font-family: "Clacon";
+    src: url(${ClaconTTF}) format("truetype");
+    font-style: normal;
+    font-weight: 400;
+  }
+  body {
+    font-family: Clacon;
+  }
+  code {
+    font-family: source-code-pro;
+  }
+`;
