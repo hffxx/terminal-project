60214c767bdc816d3b3849e148e2c919f986bd13
Maciej Hoffmann <maciej.hoffmann@blocklabs.eu>
2022-08-01 01:32:36 +0200

    commands update

--- src/data/responses.ts
+++ src/data/responses.ts
@@ -8,11 +8,13 @@ export const RESPONSES: IResponse[] = [
   {
     id: 0,
     inputs: ["command-list"],
-    response: `cls command-2 command-3 command-4 command-5`,
+    response: `author | cmd | cls `,
   },
   { id: 1, inputs: ["cls"] },
-  { id: 2, inputs: ["command-2"], response: "essa wariacie" },
-  { id: 3, inputs: ["command-3"], response: "essa wariacie" },
-  { id: 4, inputs: ["command-4"], response: "essa wariacie" },
-  { id: 5, inputs: ["command-5"], response: "essa wariacie" },
+  { id: 2, inputs: ["author"], response: "Maciej Hoffmann HFX-SOFT " },
+  {
+    id: 3,
+    inputs: ["cmd"],
+    response: "HFX-SOFT® (Version 1.0.0a). All rights reserved.",
+  },
 ];
