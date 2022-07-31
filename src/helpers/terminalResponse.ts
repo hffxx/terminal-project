bc33991b0eabdc48216ac37a6932ea79aa8df72f
Maciej Hoffmann <maciej.hoffmann@blocklabs.eu>
2022-08-01 01:38:48 +0200

    function refactor

--- src/helpers/terminalResponse.ts
+++ src/helpers/terminalResponse.ts
@@ -1,6 +1,9 @@
 import { RESPONSES } from "../data/responses";
 
 export const terminalResponse = (input: string) => {
+  if (input.trim() === "") {
+    return;
+  }
   const response = RESPONSES.find((res) => {
     return res.inputs.includes(input.toLowerCase());
   });
