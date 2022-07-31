00a81f5152d8d241ae2dac68aa27aa97bad3f527
Maciej Hoffmann <maciej.hoffmann@blocklabs.eu>
2022-08-01 01:41:11 +0200

    name refactor

--- src/helpers/terminalResponse.ts
+++ src/helpers/terminalResponse.ts
@@ -4,11 +4,11 @@ export const terminalResponse = (input: string) => {
   if (input.trim() === "") {
     return;
   }
-  const response = RESPONSES.find((res) => {
+  const responseData = RESPONSES.find((res) => {
     return res.inputs.includes(input.toLowerCase());
   });
-  if (response) {
-    return response.response;
+  if (responseData) {
+    return responseData.response;
   }
   return `"${input}" is not recognized as an internal or external command, operable program or batch file.`;
 };
